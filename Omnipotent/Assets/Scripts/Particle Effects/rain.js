e039edac90856e63f5a0ce00324b550579d3a0cd
sharky93 <rishabhr123@gmail.com>
2015-01-29 00:21:11 +0530

    Added expression recognition for triggering rain
    Rain and Rain_New are the two different particle emitters which work for
    different expressions. Have to fix Snow with the expression assigned to
    Rain_New. Some issue with Snow, changes the camera on
    activating/deactivating.

--- Omnipotent/Assets/Scripts/Particle Effects/rain.js
+++ Omnipotent/Assets/Scripts/Particle Effects/rain.js
@@ -1,16 +1,9 @@
 ﻿#pragma strict
-var str = "Hello";
 var status = true;
 function Start () {
-
+	
 }
 
 function Update () {
-	if(Input.GetKeyUp("r"))
-	{	str = "Status = " + status;
-		status = !status;
-	}
-	//gameObject.SetActive(status);
-	renderer.enabled = status;
 	
 }
---
+++ Omnipotent/Assets/Scripts/testing.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class testing : MonoBehaviour {
+	public GameObject x;
+	public bool status = false;
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("testing initiated");
+		x = GameObject.FindWithTag ("Explosion");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyUp("l")) {
+			Debug.Log ("detected L");
+			status = !status;
+			x.SetActive(status); 
+		}
+	}
+}
