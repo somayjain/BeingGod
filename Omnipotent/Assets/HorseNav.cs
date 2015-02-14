e1274f6a4d83f3824b5e6655131fc49211eea2e7
Somay <somay.jain@gmail.com>
2015-02-15 01:04:05 +0530

    Modified Terrain

---
+++ Omnipotent/Assets/HorseNav.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HorseNav : MonoBehaviour {
+
+	public GameObject[] horses;
+	public GameObject[] HD;
+
+
+
+	// Use this for initialization
+	void Start () {
+		for (int i=0; i<horses.Length; i++) {
+			horses[i].GetComponent<HorseAgent>().newPath(HD[Random.Range(0,HD.Length)].transform.position);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		for (int i=0; i<horses.Length; i++) {
+			if(horses[i].GetComponent<HorseAgent>().targetReached == true){
+				horses[i].GetComponent<HorseAgent>().newPath(HD[Random.Range(0,HD.Length)].transform.position);
+			}
+		}
+	}
+}
