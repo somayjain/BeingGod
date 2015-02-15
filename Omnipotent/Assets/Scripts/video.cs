02df364dce3167976df73c9a51d0ae1d95d3b25e
Revanth <revanth.nr@gmail.com>
2015-02-16 04:28:09 +0530

    added prologue scene

---
+++ Omnipotent/Assets/Scripts/video.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class video : MonoBehaviour {
+
+	public MovieTexture movTexture;
+	//public AudioClip audio;
+	// Use this for initialization
+	void Start () {
+		renderer.material.mainTexture = movTexture;
+		//AudioSource.PlayClipAtPoint (audio, transform.position);
+		audio.Play ();
+		movTexture.Play();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if ( !movTexture.isPlaying )
+			Application.LoadLevel(1);
+	}
+}
